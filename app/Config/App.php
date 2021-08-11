6794047bc08661e968b9f60a113032185377c94a
unknown <maikolechenique.com>
2021-08-11 09:44:18 -0400

    Actualizacion de Archivos

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost/registroMilitares/';
+	public $baseURL = 'http://192.168.12.240/';
 
 	/*
 	|--------------------------------------------------------------------------
